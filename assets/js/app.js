3b69cb41270671d32528b1b42be598d156986000
hassanraza051 <hrcollection13579@gmail.com>
2026-06-26 22:13:29 +0500

    Add mobile menu script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,7 @@
 // Jobs & Scholarships Hub
 console.log("Website Loaded");
+const menuBtn=document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click",()=>{
+alert("Mobile menu will be activated in the next update.");
+});
