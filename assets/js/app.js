baecb6cde3217e6282a398374bf4d90ac36d9b58
hassanraza051 <hrcollection13579@gmail.com>
2026-06-27 09:02:09 +0500

    Add working mobile navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,17 @@ const menuBtn=document.getElementById("menuBtn");
 menuBtn.addEventListener("click",()=>{
 alert("Mobile menu will be activated in the next update.");
 });
+// Premium Mobile Navigation
+
+const menuBtn = document.getElementById("menuBtn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle("show-menu");
+
+    if (navbar.classList.contains("show-menu")) {
+        menuBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    } else {
+        menuBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    }
+});
